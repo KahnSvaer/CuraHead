8bda41e42e0f78ee65c860d6b90b61382228b926
KahnSvaer <shivansh.pachnanda1@gmail.com>
2024-10-02 14:08:59 +0530

    Deleted Node structure Added Reusable Widgets

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,125 +1,219 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Curahead App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LandingPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: WelcomeBar(),
+      body: Column(
+        children: [
+          Expanded(child: Container()), // Empty space for main content
+          BottomNavigationBarRow(), // Bottom navigation row
+        ],
+      ), // Empty body
+    );
+  }
+}
+
+class WelcomeBar extends AppBar {
+  WelcomeBar({super.key})
+      : super(
+          backgroundColor: Colors.blue, // Set AppBar color to blue
+          elevation: 0, // Remove AppBar shadow
+          toolbarHeight:
+              150, // Increase height of AppBar to fit profile and search bar
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 30.0, // Profile icon size
+                    backgroundColor:
+                        Colors.white, // CircleAvatar background color
+                    child: Icon(Icons.person,
+                        color: Colors.blue, size: 30), // Default person icon
+                  ),
+                  SizedBox(width: 10), // Space between profile and text
+                  Column(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.start, // Align the text to the left
+                    children: [
+                      Text(
+                        'Welcome',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        '[Profile Name]',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: 20), // Space between profile and search bar
+              Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search Therapist',
+                    border: InputBorder.none,
+                    isDense: true,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.search,
+                          color: Colors.grey), // Search icon as a button
+                      onPressed: () {
+                        print("Searching");
+                      },
+                    ), // Search icon on the right
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+}
+
+class BottomNavigationBarRow extends StatelessWidget {
+  final double iconImageSize = 40;
+  final Color iconImageColor = Colors.white;
+  final Color textColor = Colors.white;
+
+  const BottomNavigationBarRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // Home button
+          NavItemButton(
+            icon: Icons.home_outlined,
+            label: 'Home',
+            iconSize: iconImageSize, // Customize size if needed
+            iconColor: iconImageColor,
+            textColor: textColor,
+            onTap: () {
+              print('Navigate to Home Page');
+            },
+          ),
+          // Chat button
+          NavItemButton(
+            icon: Icons.chat_outlined,
+            label: 'Chats',
+            iconSize: iconImageSize,
+            iconColor: iconImageColor,
+            textColor: textColor,
+            onTap: () {
+              print('Navigate to Chat Page');
+            },
+          ),
+          // Exam button (using school icon)
+          NavItemButton(
+            icon: Icons.school_outlined,
+            label: 'Assessment',
+            iconSize: iconImageSize,
+            iconColor: iconImageColor,
+            textColor: textColor,
+            onTap: () {
+              print('Navigate to Exam Page');
+            },
+          ),
+          // Settings button
+          NavItemButton(
+            icon: Icons.settings_outlined,
+            label: 'Settings',
+            iconSize: iconImageSize,
+            iconColor: iconImageColor,
+            textColor: textColor,
+            onTap: () {
+              print('Navigate to Settings Page');
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class NavItemButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final double iconSize;
+  final double textSize;
+  final Color textColor;
+  final Color iconColor;
+  final Color pressedColor;
+  final VoidCallback onTap;
+
+  const NavItemButton({
+    super.key,
+    required this.icon,
+    required this.label,
+    this.iconSize = 28.0, // Default icon size
+    this.textSize = 12.0, // Default text size
+    this.iconColor = Colors.white, // Default icon color
+    this.textColor = Colors.white, // Default text color
+    this.pressedColor = Colors.black,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: Icon(
+              icon,
+              color: iconColor,
+              size: iconSize,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            color: Colors.blue,
+            onPressed: onTap,
+          ),
+
+          SizedBox(height: 4), // Small gap between icon and label
+          Text(
+            label,
+            style: TextStyle(fontSize: textSize, color: textColor),
+          ),
+        ],
     );
   }
 }
